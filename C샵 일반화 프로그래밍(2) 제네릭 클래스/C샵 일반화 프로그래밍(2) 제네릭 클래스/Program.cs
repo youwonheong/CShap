9793defccb92f854a6bc7c6586df5a57a28456e6
youwonheong <49299979+youwonheong@users.noreply.github.com>
2020-08-28 11:49:39 +0900

    Add files via upload

---
+++ C샵 일반화 프로그래밍(2) 제네릭 클래스/C샵 일반화 프로그래밍(2) 제네릭 클래스/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C샵_일반화_프로그래밍_2__제네릭_클래스
+{
+
+    class Mylist<T>
+    {
+        private T[] array;
+        public Mylist()
+        {
+            array = new T[3];
+        }
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<T>(ref array, index + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+                array[index] = value;
+            }
+        }
+        public int Length
+        {
+            get { return array.Length; }
+        }
+        public int Rank
+        {
+            get { return array.Rank;  }
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Mylist<string> str_list = new Mylist<string>();
+            str_list[0] = "abc";
+            str_list[1] = "def";
+            str_list[2] = "ghi";
+            str_list[3] = "jkl";
+            str_list[4] = "mno";
+
+            for(int i=0; i<str_list.Length; i++)
+            {
+                Console.WriteLine(str_list[i]);
+            }
+            Console.WriteLine();
+
+            Mylist<int> int_list = new Mylist<int>();
+            for(int i = 0; i<5; i++)
+            {
+                int_list[i] = i; 
+            }
+            for(int i =0; i< int_list.Length; i++)
+            {
+                Console.WriteLine(int_list[i]);
+            }
+            
+        }
+    }
+}
---
+++ C샵 형식 매개 변수 제약/C샵 형식 매개 변수 제약/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C샵_형식_매개_변수_제약
+{
+
+    class StructArray<T> where T : struct
+    {
+        public T[] Array { get; set; }
+        public StructArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+
+    class RefArray<T> where T : class
+    {
+        public T[] Array { get; set; }
+        public RefArray(int size ){
+            Array = new T[size];
+        }
+    }
+
+    class Base { }
+    class Derived : Base { }
+    class BaseArray<U> where U : Base
+    {
+        public U[] Array { get; set; }
+        public BaseArray(int size)
+        {
+            Array = new U[size];
+        }
+        public void CopyArray<T>(T[] source) where T : U
+        {
+            source.CopyTo(Array, 0);
+        }
+    }
+
+    class Program
+    {
+        public static T CreateInstance<T>() where T : new()
+        {
+            return new T(); 
+        }
+        static void Main(string[] args)
+        {
+            StructArray<int> a = new StructArray<int>(3);
+            a.Array[0] = 0;
+            a.Array[1] = 1;
+            a.Array[2] = 2;
+
+            RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
+            b.Array[0] = new StructArray<double>(5);
+            b.Array[1] = new StructArray<double>(10);
+            b.Array[2] = new StructArray<double>(1005);
+
+            BaseArray<Base> c = new BaseArray<Base>(3);
+
+            c.Array[0] = new Base();
+            c.Array[1] = new Derived();
+            c.Array[2] = CreateInstance<Base>(); // = new Base();
+
+            BaseArray<Derived> d = new BaseArray<Derived>(3);
+            d.Array[0] = new Derived();
+            d.Array[1] = CreateInstance<Derived>();
+            d.Array[2] = CreateInstance<Derived>();
+
+            BaseArray<Derived> e = new BaseArray<Derived>(3);
+            e.CopyArray<Derived>(d.Array); 
+        }
+    }
+}
