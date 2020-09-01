37fe3189c1dfae1f76e63ea23c25d02f709f48de
youwonheong <49299979+youwonheong@users.noreply.github.com>
2020-09-01 13:34:38 +0900

    Add files via upload

---
+++ C샵 InnumerGeneric/C샵 InnumerGeneric/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C샵_InnumerGeneric
+{
+    class Mylist<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private T[] array;
+        int position = -1;
+
+        public Mylist()
+        {
+            array = new T[3];
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<T>(ref array, index + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+                array[index] = value;
+            }
+        }
+        public int Length
+        {
+            get { return array.Length; }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                yield return (array[i]);
+            }
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                yield return (array[i]);
+            }
+        }
+
+        public T Current //제네릭 전용
+        {
+            get { return array[position]; }
+        }
+
+        object IEnumerator.Current //일반
+        {
+            get { return array[position]; }
+        }
+
+        public bool MoveNext()
+        {
+            if (position == array.Length - 1)
+            {
+                Reset();
+                return false;
+            }
+            position++;
+            return (position < array.Length); // 결과과 참이면 true반환 결과가 거짓이라면 false를 반환
+        }
+        public void Reset(){
+            position = -1;
+        }
+        public void Dispose()
+        {
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mylist<string> str_list = new Mylist<string>();
+            str_list[0] = "abc";
+            str_list[1] = "def";
+            str_list[2] = "ghi";
+            str_list[3] = "jkl";
+            str_list[4] = "mno";
+
+            foreach(string str in str_list)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine();
+
+            Mylist<int> int_list = new Mylist<int>();
+            for(int i =0; i < 5; i++)
+            {
+                int_list[i] = i;
+            }
+            foreach( int no in int_list)
+            {
+                Console.WriteLine(no); 
+            }
+
+        }
+    }
+}
---
+++ C샵 일반화 컬렉션/C샵 일반화 컬렉션/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C샵_일반화_컬렉션
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            /* List<int> list = new List<int>();
+             for(int i =1; i<6; i++)
+             {
+                 list.Add(i);
+             }
+             foreach (int element in list)
+                 Console.Write(element+ " ");
+             Console.WriteLine(); 
+             list.RemoveAt(2);
+              
+             foreach (int element in list)
+                 Console.Write(element + " ");
+             Console.WriteLine();
+
+             list.Insert(2, 200);
+
+             foreach (int element in list)
+                 Console.Write(element + " ");
+             Console.WriteLine();*/ // 리스트<T> 
+
+            /*Queue<int> queue = new Queue<int>();
+            for(int i=0; i<5; i++)
+            {
+                queue.Enqueue(i + 1);
+            }
+            while (queue.Count > 0)
+                Console.WriteLine(queue.Dequeue());*/ // 큐 <T>
+
+            /* Stack<int> stack = new Stack<int>();
+
+             for(int i =0; i < 5; i++)
+             {
+                 stack.Push(i + 1);
+             }
+             while (stack.Count > 0)
+                 Console.WriteLine(stack.Pop());*/ // 스택 <T>
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            dic["하나"] = "one";
+            dic["둘"] = "two";
+            dic["셋"] = "three";
+            dic["넷"] = "four";
+            dic["다섯"] = "five";
+            Console.WriteLine(dic["하나"]);
+            Console.WriteLine(dic["둘"]);
+            Console.WriteLine(dic["셋"]);
+            Console.WriteLine(dic["넷"]);
+            Console.WriteLine(dic["다섯"]);
+             
+
+        }
+    }
+}
