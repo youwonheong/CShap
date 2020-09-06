1ee461fb26e197d2a23f15a7fc0e3c13f48a32a2
youwonheong <49299979+youwonheong@users.noreply.github.com>
2020-09-06 09:12:44 +0900

    Add files via upload

---
+++ C샵 LINQ테스트/C샵 LINQ테스트/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C샵_LINQ테스트
+{
+    class Profile
+    {
+        public string Name { get; set; }
+
+        public int Height { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrprofile =
+            {
+                new Profile(){Name= "정우성", Height =186 },
+                new Profile(){Name="김태희",Height=158},
+                new Profile(){Name="고현정",Height=172},
+                new Profile(){Name="이문세",Height=178},
+                new Profile(){Name="하동문",Height=171},
+
+            };
+            // Height가 175미만인 데이터만 추출
+            Console.WriteLine("### 무식하게 추출 ###");
+            List<Profile> profiles = new List<Profile>();
+            foreach(Profile profile in arrprofile)
+            {
+                if (profile.Height < 175)
+                    profiles.Add(profile);
+            }
+            profiles.Sort((profile1, profile2) => { return profile1.Height - profile2.Height; });
+            foreach(var profile in profiles)
+            {
+                Console.WriteLine($"{profile.Name}, {profile.Height}");
+            }
+            //LINQ를 이용해 추출
+            Console.WriteLine("### 똑똑하게 추출 ###");
+            var profiles2 = from profile in arrprofile
+                            where profile.Height < 175
+                            orderby profile.Height
+                            select profile;
+            foreach (var profile in profiles2)
+            {
+                Console.WriteLine($"{profile.Name}, {profile.Height}");
+            }
+
+        }
+    }
+}
---
+++ C샵 심플링크/C샵 심플링크/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C샵_심플링크
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile(){Name= "정우성", Height =186 },
+                new Profile(){Name="김태희",Height=158},
+                new Profile(){Name="고현정",Height=172},
+                new Profile(){Name="이문세",Height=178},
+                new Profile(){Name="하하",Height=171}
+            };
+
+            var profiles = from p in arrProfile
+                           where p.Height < 175
+                           orderby p.Name
+                           select new
+                           {
+                               Name = p.Name,
+                               InchHeight = p.Height * 0.393
+                           };
+            foreach(var profile in profiles)
+            {
+                Console.WriteLine($"{profile.Name},{profile.InchHeight}");
+            }
+
+
+        }
+    }
+}
